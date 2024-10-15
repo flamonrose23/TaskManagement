ce01df2c2ae34d80182866ab8e91dba5ad677977
VeeSimz <kayveesimba@gmail.com>
2024-10-15 17:02:22 +0200

    Connected server to the database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const { connectDb, getDb } = require('./db')
+
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running')
+let db
+
+connectDb((error) => {
+    if (!error) {
+        app.listen(3000, () => {
+            console.log('Server is running')
+        })
+        db = getDb()
+    }
 })
