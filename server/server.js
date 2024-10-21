c67f7c11bf88bed805a251d80f72254c0712161e
VeeSimz <kayveesimba@gmail.com>
2024-10-21 14:52:44 +0200

    Updated enviroment variables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express')
 const { connectDb, getDb } = require('./db')
+const port = process.env.PORT || 3000;
 const todoRoutes = require('./routes')
 const authRoutes = require('./authRoutes')
 
@@ -12,7 +15,7 @@ connectDb((error) => {
         app.use(express.json())
         app.use('/api', todoRoutes)
         app.use('/api/auth', authRoutes)
-        app.listen(3000, () => {
+        app.listen(process.env.PORT, () => {
             console.log('Server is running')
         })
         db = getDb()
