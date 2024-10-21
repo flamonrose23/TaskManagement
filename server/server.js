d6419b6079d55f62eeb773c76baaaefb53708f6d
VeeSimz <kayveesimba@gmail.com>
2024-10-21 10:57:08 +0200

    Included authentication routes in main server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { connectDb, getDb } = require('./db')
 const todoRoutes = require('./routes')
+const authRoutes = require('./authRoutes')
 
 const app = express()
 
@@ -8,7 +9,9 @@ let db
 
 connectDb((error) => {
     if (!error) {
+        app.use(express.json())
         app.use('/api', todoRoutes)
+        app.use('/api/auth', authRoutes)
         app.listen(3000, () => {
             console.log('Server is running')
         })
